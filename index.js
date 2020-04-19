0f0d84738085ddb4c3cd45a361b951f3a2f755ea
mohit61 <malikmohit619@gmail.com>
2020-04-19 18:05:42 +0530

    adding error handing for invalid input

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -35,6 +35,9 @@ app.get("/", (req, res) => {
 // getting value from key
 app.get("/getValue/:key", (req, res) => {
   let key = req.params.key;
+  if (!key) {
+    res.send("Error : Please provide a valid key");
+  }
   client.get(key, (err, rst) => {
     if (err) {
       console.log(err);
@@ -51,6 +54,9 @@ app.get("/getValue/:key", (req, res) => {
 // setting value of key
 app.post("/setValue", (req, res) => {
   let { key, value } = req.body;
+  if (!key || !value) {
+    res.send("Error : Please provide a valid key and value");
+  }
   client.set(key, value, (err, rst) => {
     if (err) {
       console.log(err);
@@ -63,22 +69,33 @@ app.post("/setValue", (req, res) => {
 // set expiration of key
 app.get("/exp/:key/:time", (req, res) => {
   let { key, time } = req.params;
-  client.expire(key, time, (err, rst) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (rst) {
-        res.send(`Done.`);
+  time = Number(time);
+  if (isNaN(time)) {
+    res.send("Error : Please provide integer value of time(in seconds)");
+  }
+  if (!key || !time) {
+    res.send("Error : Please provide a valid key and time(in seconds)");
+  } else {
+    client.expire(key, time, (err, rst) => {
+      if (err) {
+        console.log(err);
       } else {
-        res.send("no key found!");
+        if (rst) {
+          res.send(`Done.`);
+        } else {
+          res.send("no key found!");
+        }
       }
-    }
-  });
+    });
+  }
 });
 
 // get remaining time of key
 app.get("/rem/:key", (req, res) => {
   let key = req.params.key;
+  if (!key) {
+    res.send("Error : Please provide a valid key");
+  }
   client.ttl(key, (err, rst) => {
     if (err) {
       console.log(err);
@@ -97,6 +114,12 @@ app.get("/rem/:key", (req, res) => {
 // add elements in sorted set
 app.get("/zadd/:name/:value/:score", (req, res) => {
   let { name: setName, score, value } = req.params;
+  score = Number(score);
+  if (isNaN(score)) {
+    res.send("Error : Please provide a float value for score");
+  } else if (!setName || !score || !value) {
+    res.send("Error : Please provide a valid set-name and score and element");
+  }
   client.zadd(setName, score, value, (err, rst) => {
     if (err) {
       console.log(err);
@@ -113,6 +136,9 @@ app.get("/zadd/:name/:value/:score", (req, res) => {
 // get rank of element in sorted set
 app.get("/rank/:name/:value", (req, res) => {
   let { name: setName, value } = req.params;
+  if (!setName || !value) {
+    res.send("Error : Please provide a valid set-name and element!");
+  }
   client.zrank(setName, value, (err, rst) => {
     if (err) {
       console.log(err);
@@ -129,7 +155,13 @@ app.get("/rank/:name/:value", (req, res) => {
 // get the list of elements in sorted set
 app.get("/zrange/:name/:start/:stop/:withscores", (req, res) => {
   let { name: setName, start, stop, withscores } = req.params;
-  if (withscores == "true") {
+  start = Number(start);
+  stop = Number(stop);
+  if (isNaN(start) || isNaN(stop)) {
+    res.send("Error :  start and stop value must be integer");
+  } else if (!setName) {
+    res.send("Error : Please provide a valid set-name");
+  } else if (withscores == "true") {
     client.zrange(setName, start, stop, "withscores", (err, rst) => {
       if (err) {
         console.log(err);
@@ -137,7 +169,7 @@ app.get("/zrange/:name/:start/:stop/:withscores", (req, res) => {
         if (rst.length != 0) {
           res.send(rst);
         } else {
-          res.send(`empty set!`);
+          res.send(`No elements found!`);
         }
       }
     });
@@ -149,7 +181,7 @@ app.get("/zrange/:name/:start/:stop/:withscores", (req, res) => {
         if (rst.length != 0) {
           res.send(rst);
         } else {
-          res.send(`empty set!`);
+          res.send(`No elements found!`);
         }
       }
     });
@@ -159,6 +191,9 @@ app.get("/zrange/:name/:start/:stop/:withscores", (req, res) => {
 // check if key exists
 app.get("/checkKey/:key", (req, res) => {
   let key = req.params.key;
+  if (!key) {
+    res.send("Error : Please provide a valid key");
+  }
   client.exists(key, (err, rst) => {
     if (err) {
       console.log(err);
@@ -175,6 +210,9 @@ app.get("/checkKey/:key", (req, res) => {
 // delete if key exists
 app.get("/delKey/:key", (req, res) => {
   let key = req.params.key;
+  if (!key) {
+    res.send("Error : Please provide a valid key");
+  }
   client.del(key, (err, rst) => {
     if (err) {
       console.log(err);
